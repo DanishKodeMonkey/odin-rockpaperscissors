610fba12472d99dae3b66d3e4f651285f00354ed
danishkodemonkey <humberjini@hotmail.com>
2024-01-30 17:54:27 +0100

    Refactor playOnce() to accomidate new UI button selection
    Also managed to make them work well together, and print the result
    of each game to console.

--- script.js
+++ script.js
@@ -4,49 +4,54 @@ function getComputerChoice() {
   return computerChoice
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
   playerSelection = playerSelection.toLowerCase()
+  let computerSelection = getComputerChoice()
 
-  switch (playerSelection) {
-    case "rock":
-      switch (computerSelection) {
-        case "rock":
-          return "It's a draw!"
-        case "paper":
-          return "You lose!"
-        case "scissors":
-          return "You win!"
-      }
-      break
-    case "paper":
-      switch (computerSelection) {
-        case "rock":
-          return "You win!"
-        case "paper":
-          return "It's a tie!"
-        case "scissors":
-          return "You lose!"
-      }
-      break
-    case "scissors":
-      switch (computerSelection) {
-        case "rock":
-          return "You lose!"
-        case "paper":
-          return "You win!"
-        case "scissors":
-          return "It's a draw!"
-      }
-      break
-    default:
-      return "Invalid choice, please choose between rock, paper, or scissors"
+  let result = () => {
+    switch (playerSelection) {
+      case "rock":
+        switch (computerSelection) {
+          case "rock":
+            return "It's a draw!"
+          case "paper":
+            return "You lose!"
+          case "scissors":
+            return "You win!"
+        }
+        break
+      case "paper":
+        switch (computerSelection) {
+          case "rock":
+            return "You win!"
+          case "paper":
+            return "It's a tie!"
+          case "scissors":
+            return "You lose!"
+        }
+        break
+      case "scissors":
+        switch (computerSelection) {
+          case "rock":
+            return "You lose!"
+          case "paper":
+            return "You win!"
+          case "scissors":
+            return "It's a draw!"
+        }
+        break
+      default:
+        return "Invalid choice, please choose between rock, paper, or scissors"
+    }
   }
+  console.log(result())
 }
 
-function game() {
-  let playerScore = 0
-  let computerScore = 0
-  let round = 0
-}
-
-game()
+let getPlayerSelection = ""
+const buttons = document.querySelectorAll("button")
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let playerSelection = button.value
+    playRound(playerSelection)
+  })
+})
